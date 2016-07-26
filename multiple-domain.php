e1a313d7ecb56e556a2aec7bc3781d779cf4aae2
Gustavo Straube <gustavo@creativeduo.com.br>
2016-07-26 20:28:32 -0300

    Initial commit. Supports setting domains and an optional base URL for each one.

---
+++ multiple-domain.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+Plugin Name: Multiple Domain
+Plugin URI:  http://creativeduo.com.br
+Description: This plugin allows you to have multiple domains in a single 
+             WordPress installation and enables custom redirects for each 
+             domain.
+Version:     0.1
+Author:      Gustavo Straube (Creative Duo)
+Author URI:  http://creativeduo.com.br
+License:     GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
+class MultipleDomain
+{
+
+    private $host = null;
+
+    private $domains = [];
+
+    public function __construct()
+    {
+        if (!empty($_SERVER['HTTP_HOST'])) {
+            $this->host = $_SERVER['HTTP_HOST'];
+        }
+        $this->domains = get_option('multiple-domain-domains');
+        if (!is_array($this->domains)) {
+            $this->domains = [];
+        }
+    }
+
+    public function setup()
+    {
+        add_action('init', [ $this, 'redirect' ]);
+        add_action('admin_init', [ $this, 'settings' ]);
+        add_filter('option_home', [ $this, 'filterHome' ]);
+    }
+
+    public function redirect()
+    {
+        if (!empty($this->domains[$this->host])) {
+            $base = $this->domains[$this->host];
+            if (strpos($_SERVER['REQUEST_URI'], $base) !== 0) {
+                wp_redirect(home_url($base));
+                exit;
+            }
+        }
+    }
+
+    public function filterHome($home)
+    {
+        if (isset($this->domains[$this->host])) {
+            $parts = parse_url($home);
+            $search = $parts['host'];
+            if (!empty($parts['port'])) {
+                $search .= ':' . $parts['port'];
+            }
+            $home = str_replace($search, $this->host, $home);
+        }
+        return $home;
+    }
+
+    public function settings()
+    {
+        add_settings_section('multiple-domain', 'Multiple Domain', [ $this, 'settingsHeading' ], 'general');
+        add_settings_field('multiple-domain-domains', 'Domains', [ $this, 'settingsField' ], 'general', 'multiple-domain');
+        register_setting('general', 'multiple-domain-domains', [ $this, 'sanitizeSettings' ]);
+    }
+
+    public function sanitizeSettings($value)
+    {
+        $domains = [];
+        foreach (explode("\n", $value) as $row) {
+            if (empty($row)) {
+                continue;
+            }
+            if (strpos($row, ',') !== false) {
+                list($host, $base) = explode(',', $row);
+                $domains[$host] = $base;
+            } else {
+                $domains[$row] = null;
+            }
+        }
+        return $domains;
+    }
+
+    public function settingsHeading()
+    {
+        echo '<p>' . __('You can use multiple domains in your WordPress defining them below. It\'s possible to limit the access for each domain to a base URL.', 'multiple-domain') . '</p>';
+    }
+
+    public function settingsField()
+    {
+        $value = '';
+        foreach ($this->domains as $host => $base) {
+            if (!empty($value)) {
+                $value .= "\n";
+            }
+            $value .= $host;
+            if (!empty($base)) {
+                $value .= ',' . $base;
+            }
+        }
+        echo '<textarea id="multiple-domain-domains" name="multiple-domain-domains" class="large-text code" rows="5">' . $value . '</textarea>'
+            . '<p class="description">' . __('Add one domain per line, without protocol. To define a base URL restriction, add it in the same line as the domain after a comma. All requests to a URL under the domain that don\'t start with the base URL, will be redirected to the base URL. Example: <code>example.com,/base/path</code>', 'multiple-domain') . '</p>';
+    }
+}
+
+$domains = new MultipleDomain();
+$domains->setup();
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Multiple Domain ===
+Contributors: gustavostraube
+Tags: multiple, domains, redirect
+Requires at least: 4.0
+Tested up to: 4.5.3
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin allows you to have multiple domains in a single Wordpress installation and enables custom redirects for each 
+domain.
+
+== Description ==
+
+Multiple Domain allows you having more than one domain in a single WordPress installation. Differently than WordPress 
+Multisite (MU), this plugin doesn't support a different theme or advanced customizations for each domain. It's only 
+intended to enable constant navigation under a different domain than the one you set under WordPress options.
+
+Without this plugin, if you have more than one domain set in your host, when a user access the website from a domain 
+that is not the one defined in WordPress settings, all links will point to the default domain. After installing this 
+plugin, you can set other domains and all links will be updated on the flow to use those domains.
+
+This way, the user navigation will be end-to-end under the same domain.
+
+You can also set an optional base URL. If you want to only a set of URL's to be accessed under a given domain, you can 
+use this restriction.
+
+== Installation ==
+
+Follow the steps below to install the plugin:
+
+1. Upload the plugin files to the `/wp-content/plugins/multiple-domain` directory, or install the plugin through the 
+    WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress
+3. Use the Settings -> General screen to configure your additional domains
+
+== Frequently Asked Questions ==
+
+= Does this plugin set extra domains within my host? =
+
+No. You have to set additional domains, DNS and everything to use this domain.
+
+= Can I have a different theme/content/plugins for each domain? =
+
+Nope. If you want a complex set up like this, you may be interested on WordPress Multisite. It's delivered with every 
+WordPress installation since 3.0, you can find more info here: [https://codex.wordpress.org/Create_A_Network].
+
+== Screenshots ==
+
+== Changelog ==
+
+= 0.1 =
+This is the inital version. It supports setting domains and an optional base URL for each one.
+
+== Upgrade Notice ==
+
